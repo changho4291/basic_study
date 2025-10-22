a014e2476c1b04c27a8f5b111bebf2a68cdfabf4
changho4291 <changho4291@gmail.com>
2025-10-22 23:41:27 +0900

    힌트 보고 문제 풀었음 (백준 1003)

--- 02_coding_test/032_backjun_1003/main.cpp
+++ 02_coding_test/032_backjun_1003/main.cpp
@@ -1,34 +1,30 @@
 #include <cstdio>
 #include <vector>
-#include <cstdint>
-
-int fibonacci(int n, uint32_t& zero, uint32_t& one) {
-    if (n == 0) {
-        zero++;
-        return 0;
-    } else if (n == 1) {
-        one++;
-        return 1;
-    } else {
-        return fibonacci(n-1, zero, one) + fibonacci(n-2, zero, one);
-    }
-}
 
 int main(int argc, char const *argv[]) {
-    int t;
+    int t, n, fibo[41] = {0, 1};
+
+    for(int i = 2; i < 41; i++) {
+        fibo[i] = fibo[i - 2] + fibo[i - 1];
+    }
+
     scanf("%d", &t);
-    std::vector<uint32_t> zero_arr;
+    std::vector<int> zero_arr;
     zero_arr.reserve(t);
 
-    std::vector<uint32_t> one_arr;
+    std::vector<int> one_arr;
     one_arr.reserve(t);
     
     for (int i = 0; i < t; i++) {
-        int n;
-
         scanf("%d", &n);
 
-        fibonacci(n, zero_arr[i], one_arr[i]);
+        if (!n) {
+            zero_arr[i] = 1;
+        } else {
+            zero_arr[i] = fibo[n-1];
+        }
+
+        one_arr[i] = fibo[n];
     }
 
     for (int i = 0; i < t; i++) {
