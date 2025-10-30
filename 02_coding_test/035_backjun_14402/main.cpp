890f49f37ec6794de78afb29ce7d5d156518123d
changho4291 <changho4291@gmail.com>
2025-10-30 18:46:17 +0900

    백준 14402 코드 최적화

--- 02_coding_test/035_backjun_14402/main.cpp
+++ 02_coding_test/035_backjun_14402/main.cpp
@@ -4,35 +4,30 @@
 #include <cstring>
 
 int main(int argc, char const *argv[]) {
-    std::cin.tie(NULL);
-	std::ios_base::sync_with_stdio(false);
-
     int q, cnt = 0;
     std::cin >> q;
 
-    std::map<std::string, std::vector<std::string>> records;
+    std::map<std::string, std::vector<char>> records;
 
     for (int i = 0; i < q; i++) {
         std::string name;
-        std::string oper;
+        char oper;
 
         std::cin >> name >> oper;
 
         if (records.find(name) != records.end()) {
-            std::string temp = records[name].back();
-            if(oper == "-") {
-                if (temp == "-") { cnt++; }
+            char temp = records[name].back();
+            if(oper == '-') {
+                if (temp == '-') { cnt++; }
                 else { cnt--; }
             } else { cnt++; }
             records[name].push_back(oper);
         } else {
-            std::vector<std::string> reco;
-            reco.push_back(oper);
-            records.insert(make_pair(name, reco));
+            records[name].push_back(oper);
             cnt++;
         }
     }
 
-    printf("%d", cnt);
+    printf("%d\n", cnt);
     return 0;
 }
