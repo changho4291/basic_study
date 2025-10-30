540d3017772020741778f992a2e7d70df7d99602
changho4291 <changho4291@gmail.com>
2025-10-30 18:32:08 +0900

    백준 14402 반례 전부 찾아서 고쳤는데 왜 틀렸다고 하는거지

--- 02_coding_test/035_backjun_14402/main.cpp
+++ 02_coding_test/035_backjun_14402/main.cpp
@@ -3,49 +3,36 @@
 #include <vector>
 #include <cstring>
 
-typedef struct {
-    std::string oper;
-    int in_count = 0;
-} Work;
-
 int main(int argc, char const *argv[]) {
+    std::cin.tie(NULL);
+	std::ios_base::sync_with_stdio(false);
+
     int q, cnt = 0;
     std::cin >> q;
 
-    std::map<std::string, std::vector<Work>> records;
+    std::map<std::string, std::vector<std::string>> records;
 
     for (int i = 0; i < q; i++) {
         std::string name;
         std::string oper;
 
-        std::cin >> name;
-        std::cin >> oper;
+        std::cin >> name >> oper;
 
-        Work newWork;
-        newWork.oper = oper;
         if (records.find(name) != records.end()) {
-            Work temp = records[name].back();
+            std::string temp = records[name].back();
             if(oper == "-") {
-                if (temp.oper == "-") { cnt++; }
-                newWork.in_count = 0;
-            } else {
-                if (temp.oper == "+") { 
-                    if (temp.in_count == 1) { cnt += 2; }
-                    else { cnt++; }
-                    newWork.in_count = temp.in_count + 1;
-                }
-            }
-            records[name].push_back(newWork);
+                if (temp == "-") { cnt++; }
+                else { cnt--; }
+            } else { cnt++; }
+            records[name].push_back(oper);
         } else {
-            if(oper == "-") { cnt++; }
-            else { newWork.in_count = 1; }
-            std::vector<Work> reco;
-            reco.push_back(newWork);
+            std::vector<std::string> reco;
+            reco.push_back(oper);
             records.insert(make_pair(name, reco));
+            cnt++;
         }
     }
 
     printf("%d", cnt);
-
     return 0;
 }
