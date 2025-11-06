9ace89a5dc2a3de104b35f85399d1f16673e0f14
changho4291 <changho4291@gmail.com>
2025-11-06 22:29:44 +0900

    백준 1463 답보고 품 ㅠ

--- 02_coding_test/068_backjun_1463/main.cpp
+++ 02_coding_test/068_backjun_1463/main.cpp
@@ -1,99 +1,35 @@
 #include <iostream>
 #include <vector>
-#include <cstdint>
+#include <algorithm>
 
 using namespace std;
-
-bool three(int& num, int& cnt, const vector<uint32_t>& mins);
-bool two(int& num, int& cnt, const vector<uint32_t>& mins);
-
-// 3으로만 나눠지는거
-bool three(int& num, int& cnt, const vector<uint32_t>& mins) {
-    if (num % 3 == 0) {
-
-        int tempCnt2 = cnt;
-        int tempNum2 = num;
-        two(tempNum2, tempCnt2, mins);
-
-        num /= 3;
-        if(num != 1) { 
-            cnt = mins[num - 1]; 
-            num = 1;
-        }
-        cnt++;
-
-        if (tempCnt2 < cnt) { cnt = tempCnt2; }
-
-        return true;
-    } else if (num % 3 == 1) {
-        // NUM = 1 박기 전에 2로도 풀어봐야함!
-        int tempCnt = cnt;
-        int tempNum = num;
-        two(tempNum, tempCnt, mins);
-
-        num--;
-        cnt++;
-        cnt += mins[num - 1];
-
-        if (tempCnt < cnt) { cnt = tempCnt; }
-
-        num = 1;
-        return true;
-    }
-    return false;
-}
-
-// 2로만 나눠지는거
-bool two(int& num, int& cnt, const vector<uint32_t>& mins) {
-    if (num % 2 == 0) {
-        num /= 2;
-        if(num != 1) { 
-            cnt = mins[num - 1]; 
-            num = 1;
-        }
-        cnt++;
-        return false;
-    } else if (num % 2 == 1) {
-        num--;
-        cnt++;
-        cnt += mins[num - 1];
-        num = 1;
-        return false;
-    }
-    return true;
-}
 
 int main(int argc, char const *argv[]) {
     cin.tie(0);
     cout.tie(0);
     ios_base::sync_with_stdio(0);
 
-    uint32_t N;
+    int N;
     cin >> N;
     
-    vector<uint32_t> mins(N, -1);
+    vector<int> mins(N + 1, 0);
 
-    for(int i = 0; i < N; i++) {
-        int num = i + 1, cnt = 0;
-        bool check = true;
-        while ( num != 1 ) {
-            if (check == true) {
-                check = three(num, cnt, mins);
-                if(num == 1) {break;}
-            } else {
-                check = two(num, cnt, mins);
-            }
+    for(int i = 2; i <= N; i++) {
+        // 1을 뺀 경우의 수
+        mins[i] = mins[i - 1] + 1;
+        
+        // 3으로 나눈 경우의 수
+        if (i % 3 == 0) {
+            mins[i] = min(mins[i], mins[i/3] + 1);
         }
-        mins[i] = cnt;
-        check = true;
+
+        // 2로 나눈 경우의 수
+        if (i % 2 == 0) {
+            mins[i] = min(mins[i], mins[i/2]  + 1);
+        }
     }
 
-    cout << mins[N - 1] <<"\n";
-
-    // for(auto& m : mins) {
-    //     cout << m << " ";
-    // }
-    // cout << "\n";
+    cout << mins[N] <<"\n";
     return 0;
 }
 
