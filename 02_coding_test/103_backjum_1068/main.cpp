39ed647c91604d97925a077ad8296669156d5307
changho4291 <changho4291@gmail.com>
2025-11-26 00:04:24 +0900

    백준 풀다 말았음

---
+++ 02_coding_test/103_backjum_1068/main.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(0);
+    
+    int n;
+    cin >> n;
+
+    vector<vector<int>> tree(n, vector(n, -1));
+
+    for(int i = 0; i < n; i++) {
+        cin >> 
+    }
+
+    return 0;
+}
