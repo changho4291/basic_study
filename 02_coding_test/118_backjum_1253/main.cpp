fd88710b19387c1c736d5faafc92ca27308bdd26
changho4291 <changho4291@gmail.com>
2025-12-06 23:15:55 +0900

    좋다를 풀었당!

--- 02_coding_test/118_backjum_1253/main.cpp
+++ 02_coding_test/118_backjum_1253/main.cpp
@@ -22,34 +22,36 @@ int main(int argc, char const *argv[]) {
     sort(nums.begin(), nums.end());
     int result = 0;
 
+    // 뒤에서부터 타겟을 찾는다.
+    // 타겟보다 큰 수일 경우 빼서 구할 수 있는 경우
+    // 타겠보다 작을 경우 더해서 구할 수 있는 경우
+    // 다른 주소로 타겠과 같은 경우는 0이 있는 경우, 타겟이 0인 경우엔 3개 이상 있을 때만
+
     for (int i = n - 1; i >= 0; i--) {
-        // 0이 있고 타겟 숫자가 복수개인 경우
-        if (binary_search(nums.begin(), nums.end(), 0)) {
-            int c = 2;
-            if (nums[i] == 0) { c = 3; }
-            if(upper_bound(nums.begin(), nums.end(), nums[i]) - lower_bound(nums.begin(), nums.end(), nums[i]) >= c) { 
-                result++; 
-                continue;
-            }
-        }
-
         bool isEven = false;
-
         if (nums[i] % 2 == 0) { isEven = true; }
 
-        int j = i - 1;
-        while (nums[j] >= nums[i]/2 && nums[j] < nums[i] && j >= 0) {
-            if (isEven && nums[j] == nums[i]/2) {
-                if(upper_bound(nums.begin(), nums.end(), nums[j]) - lower_bound(nums.begin(), nums.end(), nums[j]) >= 2) { result++; };
+        for (int j = n - 1; nums[j] >= nums[i]/2 && j > 0; j--) {
+            if (j == i) { continue; }
+
+            if (isEven && (nums[j] == nums[i]/2)) {
+                int c = 2;
+                if (nums[i] == 0) { c = 3; }
+                if(upper_bound(nums.begin(), nums.end(), nums[j]) - lower_bound(nums.begin(), nums.end(), nums[j]) >= c) { result++; }
                 break;
             }
 
             int64_t k = nums[i] - nums[j] ;
-            if (binary_search(nums.begin(), nums.end(), k)) {
+            
+            if (nums[j] == 0) {
+                if(upper_bound(nums.begin(), nums.end(), nums[i]) - lower_bound(nums.begin(), nums.end(), nums[i]) >= 2) { 
+                    result++; 
+                    break;
+                }
+            } else if (binary_search(nums.begin(), nums.end(), k)) {
                 result++; 
                 break;
             }
-            j--;
         }
     }
 
