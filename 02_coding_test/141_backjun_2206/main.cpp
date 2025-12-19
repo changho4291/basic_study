daf4c6a512e528964d741e73fc4170fcc3bdbdcf
changho4291 <changho4291@gmail.com>
2025-12-19 22:25:55 +0900

    벽부수기 두번째 실패 (시간초과)

--- 02_coding_test/141_backjun_2206/main.cpp
+++ 02_coding_test/141_backjun_2206/main.cpp
@@ -10,8 +10,6 @@ typedef struct {
     int d;
 } Data;
 
-void back(vector<vector<char>>& board, int startX, int startY);
-
 int main () {
     cin.tie(0);
     cout.tie(0);
@@ -23,46 +21,54 @@ int main () {
     int n, m;
     cin >> n >> m;
 
-    vector<vector<char>> board(n, vector<char>(m));
-    vector<vector<bool>> isVisited(n, vector<bool>(m, false));
+    vector<vector<char>> board(n, vector<char>(m, 0));
+    vector<pair<int, int>> walls;
     queue<Data> bfs;
 
     for(int i = 0; i < n; i++) {
         for (int j = 0; j < m; j++) {
             cin >> board[i][j];
+            if (board[i][j] == '1') {
+                walls.push_back({i, j});
+            }
         }
     }
 
-    
+    int result = -1;
+    for (auto& w : walls) {
+        board[w.first][w.second] = '0';
+        vector<vector<bool>> isVisited(n, vector<bool>(m, false));
+        bfs.push({0, 0, 1});
+        isVisited[0][0] = true;
 
-    bfs.push({0, 0, 1});
-    isVisited[0][0] = true;
+        bool isRun = true;
+        while (!bfs.empty()) {
+            auto current = bfs.front();
+            bfs.pop();
 
-    while (!bfs.empty()) {
-        auto current = bfs.front();
-        bfs.pop();
+            for (int i = 0; i < 4; i++) {
+                int x = current.x + moveX[i];
+                int y = current.y + moveY[i];
 
-        for (int i = 0; i < 4; i++) {
-            int x = current.x + moveX[i];
-            int y = current.y + moveY[i];
+                if (x < 0 || y < 0 || x >= n || y >= m) { continue; }
+                if (isVisited[x][y] || (board[x][y] == '1')) { continue; }
+                
+                if ((x == n -1) && (y == m-1)) {
+                    if ( (result == -1) || result > current.d + 1 ) { result = current.d + 1; }
+                    isRun = false;
+                    break;
+                }
 
-            if (x < 0 || y < 0 || x >= n || y >= m) { continue; }
-            if (isVisited[x][y] || (board[x][y] == '1')) { continue; }
-            
-            if ((x == n -1) && (y == m-1)) {
-                cout << current.d + 1 << "\n";
-                return 0;
+                bfs.push({x, y, current.d + 1});
+                isVisited[x][y] = true;
             }
 
-            bfs.push({x, y, current.d + 1});
-            isVisited[x][y] = true;
+            if(!isRun) { break; }
         }
+        board[w.first][w.second] = '1';
     }
-    cout << "-1\n";
+
+    cout << result << "\n";
 
     return 0;
-}
-
-void back(vector<vector<char>>& board, int startX, int startY) {
-
 }
