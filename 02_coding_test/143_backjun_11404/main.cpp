71d236318960f876f30660e4fcdca2d298b3927c
changho4291 <changho4291@gmail.com>
2025-12-21 18:13:03 +0900

    플로이드 오답

---
+++ 02_coding_test/143_backjun_11404/main.cpp
@@ -0,0 +1,45 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+#define INF 0x7FFFFFFF
+
+int main () {
+    cin.tie(0);
+    cout.tie(0);
+    ios_base::sync_with_stdio(0);
+
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<int>> floydTable(n, vector<int>(n, INF));
+
+    int j = INF;
+
+    int a, b, c;
+    for (int i = 0; i < m; i++) {
+        cin >> a >> b >> c;
+        if (floydTable[a - 1][b - 1] > c) { floydTable[a- 1][b - 1] = c; }
+    }
+
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < n; j++) {
+            for (int k = 0; k < n; k++) {
+                if (j == k) { floydTable[j][k] = 0; continue;}
+                if (floydTable[j][i] == INF || floydTable[i][k] == INF) { continue; }
+                floydTable[j][k] = min(floydTable[j][i] + floydTable[i][k], floydTable[j][k] );
+            }
+        }
+    }
+
+    for (auto& v : floydTable) {
+        for (auto& i : v) {
+            cout << i << " ";
+        }
+        cout << "\n";
+    }
+
+    return 0;
+}
