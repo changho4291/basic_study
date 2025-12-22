1a8a2ae0d3ce77ce544311f17b2126fef28b7de4
changho4291 <changho4291@gmail.com>
2025-12-22 20:46:11 +0900

    최소거리 해결 다익스트라 조낸 어렵다.

--- 02_coding_test/145_backjun_1753/main.cpp
+++ 02_coding_test/145_backjun_1753/main.cpp
@@ -5,19 +5,7 @@
 
 using namespace std;
 
-#define INF -1
-
-typedef struct Data_ {
-    int node;
-    int next;
-    uint32_t value;
-
-    Data_(int node_, int next_, uint32_t value_) : node(node_), next(next_), value(value_) {}
-
-    bool operator<(const Data_ d) const {
-        return this->value > d.value;
-    }
-} Data;
+const uint64_t INF = (1ull << 62);
 
 int main(int argc, char const *argv[]) {
     cin.tie(0);
@@ -27,39 +15,39 @@ int main(int argc, char const *argv[]) {
     int n, e, k;
     cin >> n >> e >> k;
 
-    vector<uint32_t> path_value(n, INF);
-    vector<vector<Data>> graph(n);
-    priority_queue<Data> pque;
+    vector<uint64_t> path_value(n, INF);
+    vector<vector<pair<int, uint32_t>>> graph(n);
+    priority_queue<pair<uint64_t, int>, vector<pair<uint64_t, int>>, greater<pair<uint64_t, int>>> pque;
 
     int u, v;
     uint32_t w;
     for(int i = 0; i < e; i++) {
         cin >> u >> v >> w;
-        graph[u - 1].push_back({u - 1, v - 1, w});
-        if (u == k) {
-            pque.push({u - 1, v - 1, w});
-        }
+        graph[u - 1].push_back({v - 1, w});
     }
 
     path_value[k - 1] = 0;
+    pque.push({0, k - 1});
 
     while (!pque.empty()) {
-        auto current = pque.top();
+        auto [f, s] = pque.top();
         pque.pop();
+        if (f != path_value[s]) { continue; }
 
-        uint32_t tmp = path_value[current.node] + current.value;
-        if (path_value[current.next] > tmp) { 
-            path_value[current.next] = tmp; 
-            
-            for(auto& d : graph[current.next]) { pque.push(d); }
+        for(auto& [no, val] : graph[s]) {
+            uint64_t tmp = f + val;
+            if (path_value[no] > tmp) { 
+                path_value[no] = tmp; 
+                pque.push({tmp, no});
+            }
         }
     }
     
     for(auto& p : path_value) {
-        if (p != -1) {
-            cout << p << " ";
-        } else {
+        if (p == INF) {
             cout << "INF ";
+        } else {
+            cout << p << " ";
         }
     }
     cout << "\n";
