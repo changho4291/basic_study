34362e4814b34a2ceb21fd12fc4849b2f64ff5c2
changho4291 <changho4291@gmail.com>
2025-12-26 23:41:41 +0900

    백준 브론즈

---
+++ 02_coding_test/151_backjun_25083/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main(int argc, char const *argv[]) {
+    cin.tie(0);
+    cout.tie(0);
+    
+    string s = 
+"         ,r'\"7\n"
+"r`-_   ,'  ,/\n"
+" \\. \". L_r'\n"
+"   `~\\/\n"
+"      |\n"
+"      |\n";
+
+    cout << s;
+    return 0;
+}
