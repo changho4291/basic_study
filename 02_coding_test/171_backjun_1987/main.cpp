f2246bee6ae952a4d01e4a7b0681c766da9280e8
changho4291 <changho4291@gmail.com>
2026-01-14 20:09:13 +0900

    중간 저장

--- 02_coding_test/171_backjun_1987/main.cpp
+++ 02_coding_test/171_backjun_1987/main.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <vector>
 #include <queue>
+#include <cstdint>
 
 using namespace std;
 
@@ -8,6 +9,7 @@ typedef struct {
     int x;
     int y;
     int cnt;
+    uint32_t alpahFlag;
 } Data;
 
 int main(int argc, char const *argv[]) {
@@ -15,8 +17,7 @@ int main(int argc, char const *argv[]) {
 
     cin >> r >> c;
     vector<string> board(r, string(c, ' '));
-    vector<vector<bool>> isVisited(r, vector<bool>(c, false));
-    vector<bool> alphaUsed(26, false);
+    vector<vector<vector<bool>>> isVisited(r,  vector<vector<bool>>(c, vector<bool>(26, false)));
     queue<Data> bfs;
 
     int moveX[] = {0, 1, 0, -1};
@@ -28,9 +29,8 @@ int main(int argc, char const *argv[]) {
         }
     }
 
-    bfs.push({0, 0, 1});
-    isVisited[0][0] = true;
-    alphaUsed[board[0][0] - 'A'] = true;
+    bfs.push({0, 0, 1, (uint32_t)(1 << (board[0][0] - 'A'))});
+    isVisited[0][0][board[0][0] - 'A'] = true;
 
     int result = 0;
     while (!bfs.empty()) {
@@ -42,9 +42,12 @@ int main(int argc, char const *argv[]) {
             int y = current.y + moveY[i];
 
             if (x < 0 || y < 0 || x >= r || y >= r) { continue; }
-            if (alphaUsed[board[x][y] - 'A'] || isVisited[x][y]) { continue; }
+            if (isVisited[x][y][board[x][y] - 'A']) { continue; }
 
-            bfs.push({x, y, current.cnt + 1});
+            uint32_t tmp = current.alpahFlag;
+            tmp |= (uint32_t)(1 << (board[x][y] - 'A'));
+
+            bfs.push({x, y, current.cnt + 1, tmp});
             isVisited[x][y] = true;
             alphaUsed[board[x][y] - 'A'] = true;
             result = current.cnt + 1;
