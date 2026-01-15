cf9236282f0f44f999260e558d9c99e6bb01a876
changho4291 <changho4291@gmail.com>
2026-01-15 10:29:59 +0900

    앞파벳 해결

--- 02_coding_test/171_backjun_1987/main.cpp
+++ 02_coding_test/171_backjun_1987/main.cpp
@@ -1,81 +1,62 @@
 #include <iostream>
 #include <vector>
 #include <queue>
-#include <cstring>
+#include <cstdint>
 
 using namespace std;
+
+int r, c;
+char board[20][20];
+
+int moveX[] = {0, 1, 0, -1};
+int moveY[] = {1, 0, -1, 0};
+
+int cnt = 1;
+int result = 0;
 
 typedef struct {
     int x;
     int y;
-    int cnt;
-    bool alphaUsed[26] = {false, };
 } Data;
 
+uint32_t alphaUsed;
+
+void backTrack(Data data);
+
 int main(int argc, char const *argv[]) {
-    int r, c;
-
     cin >> r >> c;
-    vector<string> board(r, string(c, ' '));
-    // vector<vector<bool>> isVisited(r, vector<bool>(c, false));
-    // vector<bool> alphaUsed(26, false);
-    queue<Data> bfs;
-
-    int moveX[] = {0, 1, 0, -1};
-    int moveY[] = {1, 0, -1, 0};
 
     for (int i = 0; i < r; i ++) {
+        getchar();
         for (int j = 0; j < c; j++) {
-            cin >> board[i][j];
+            board[i][j] = getchar();
         }
     }
-    
 
-    Data first = {
-        .x = 0,
-        .y = 0,
-        .cnt = 1,
-    };
-    first.alphaUsed[board[0][0] - 'A'] = true;
-    bfs.push(first);
-    // isVisited[0][0] = true;
-    // alphaUsed[board[0][0] - 'A'] = true;
+    alphaUsed |= ((uint32_t)1 << (board[0][0] - 'A'));
+    backTrack({0, 0});
 
-    int result = 0;
-    while (!bfs.empty()) {
-        auto current = bfs.front();
-        bfs.pop();
-
-        // for (int j = 0; j < 26; j++) {
-        //     cout << current.alphaUsed[j] << " ";
-        // }
-        // cout << "\n";
-
-        // int stop;
-        // cin >> stop;
-
-        for (int i = 0; i < 4; i++) {
-            int x = current.x + moveX[i];
-            int y = current.y + moveY[i];
-
-            if (x < 0 || y < 0 || x >= r || y >= r) { continue; }
-            if (current.alphaUsed[board[x][y] - 'A']) { continue; }
-            // if (alphaUsed[board[x][y] - 'A'] || isVisited[x][y]) { continue; }
-
-
-            Data input = {
-                .x = x,
-                .y = y,
-                .cnt = current.cnt + 1,
-            };
-            memcpy(input.alphaUsed, current.alphaUsed, 26);
-            input.alphaUsed[board[x][y] - 'A'] = true;
-            bfs.push(input);
-            // isVisited[x][y] = true;
-            result = current.cnt + 1;
-        }
-    }
-    
+    if (result == 0) { result = 1; }
     cout << result << "\n";
+
     return 0;
+}
+
+void backTrack(Data data) {
+    for (int i = 0; i < 4; i++) {
+        int x = data.x + moveX[i];
+        int y = data.y + moveY[i];
+
+        if (x < 0 || y < 0 || x >= r || y >= c) {continue;}
+        if (alphaUsed & ((uint32_t)1 << (board[x][y] - 'A'))) {
+            result = max(result, cnt);
+            continue; 
+        }
+
+        cnt++;
+        alphaUsed |= ((uint32_t)1 << (board[x][y] - 'A'));
+        backTrack({x, y});
+        cnt--;
+        alphaUsed &= ~((uint32_t)1 << (board[x][y] - 'A'));
+    }
 }
