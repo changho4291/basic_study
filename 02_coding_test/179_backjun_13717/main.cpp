bdc5f8c7e8955f0736a7450e2a2e4a4e7bc5b08f
changho4291 <changho4291@gmail.com>
2026-02-05 19:44:28 +0900

    포멧몬고 오답

---
+++ 02_coding_test/179_backjun_13717/main.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+typedef struct {
+    string name;
+    int need;
+    int have;
+} PokeMon;
+
+int main(int argc, char const *argv[]) {
+    cin.tie(0); cout.tie(0);
+    ios_base::sync_with_stdio(0);
+
+    int n;
+    cin >> n;
+    vector<PokeMon> pokemons(n);
+
+    string goat;
+    int goatNum = -1;
+    int totalEvolution = 0;
+    for (auto& p : pokemons) {
+        cin >> p.name >> p.need >> p.have;
+
+        int evolution = (p.have / p.need);
+        p.have = (p.have - (evolution * p.need)) + (evolution * 2);
+        evolution += (p.have / p.need);
+        totalEvolution += evolution;
+
+        if (evolution > goatNum) {
+            goatNum = evolution;
+            goat = p.name;
+        }
+    }
+
+    cout << totalEvolution << "\n";
+    cout << goat << "\n";
+
+    return 0;
+}
